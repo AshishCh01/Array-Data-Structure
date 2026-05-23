ebb1d88c328e38390da256fa4dce0cc23d109b67
ashish-01-alpha <ashishbittu260@gmail.com>
2026-05-23 23:51:03 +0530

    4 sum updated

---
+++ Hard/4-sum/optimized.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    int n = nums.size();
+    set<vector<int>> st;
+    for (int i=0; i<n; i++) {
+        for (int j= i+1; j<n; j++) {
+            set<long long> hashset;
+            for (int k=j+1; k<n;k++) {
+                long long sum = nums[i] +nums[j];
+                sum += nums[k];
+                long long fourth = target - (sum);
+                if (hashset.find(fourth) != hashset.end()) {
+                    vector<int> temp = {nums[i], nums[j], nums[k], nums[fourth]};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+                hashset.insert(nums[k]);
+            }
+        }
+    }
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+}
+int main()
+{
+    vector<int> nums = {2,2,2,2,2};
+    int target = 8;
+     vector<vector<int>> ans = fourSum(nums, target);
+     for(auto it : ans) {
+        for(auto x : it) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
